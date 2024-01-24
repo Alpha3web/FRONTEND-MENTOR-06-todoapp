6bf202b4e52533bc67016e238ca61b5d97b709b9
Akeelah <akeelahsmart@gmail.com>
2024-01-24 18:01:41 +0100

    Modify js and css

--- public/static_js/index.js
+++ public/static_js/index.js
@@ -1,136 +1,62 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+
 const todos = document.querySelector('.todo-list');
 const callToAction = document.querySelector('.call-to-action');
 const anchor = document.querySelectorAll('.call-to-action a');
-const modes = document.querySelector('header');
+const modeIcons = document.querySelector('.mode-icons');
 
+// To apply theme
+const pageBody = document.querySelector('body');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const todoItems = document.querySelectorAll('.todo-item');
+const wrapper = document.querySelector('.wrapper')
 
+const changeTheme = () => {
+    pageBody.classList.toggle('dark-body')
+    form.classList.toggle('dark-form')
+    input.classList.toggle('dark-input')
+    callToAction.classList.toggle('dark-call-to-action')
+    wrapper.classList.toggle('dark-wrapper')
 
-// themes
-
-let style = document.createElement('style');
-
-const jsStyle = () => {
-    if (document.head.lastElementChild.tagName === 'STYLE') {
-        document.head.lastElementChild.remove();
-        document.head.insertAdjacentHTML("beforeend", style);
-    } else {
-        document.head.insertAdjacentHTML("beforeend", style);
-    }
+    todoItems.forEach(item => {
+        item.classList.toggle('dark-todo-item')
+    })
 }
 
+// if (input.classList.contains('for-light-theme')) {
+//     input.classList.replace('for-light-theme', 'for-dark-theme');
+// } else {
+//     input.classList.add('for-dark-theme');
+// }
+// anchor.forEach(a => {
 
-// dark theme function
-const darkTheme = () => {
-    style = `
-    <style>
-    body{
-    background-color: var(--Very-Dark-Blue);
-    background-image: url(images/bg-desktop-dark.jpg);
-    }
-    form {
-    background-color: var(--Very-Dark-Desaturated-Blue);
-    }
-    
-    input {
-    background: var(--Very-Dark-Desaturated-Blue);
-    }
+//     if (a.classList.contains('light-anchor')) {
+//         a.classList.replace('light-anchor', 'dark-anchor');
+//     } else {
+//         a.classList.add('dark-anchor');
+//     }
+// });
 
-    .todo-list > li{
-        color: var(--Light-Grayish-Blue);
-        border-bottom: 1px solid var(--Very-Dark-Grayish-Blue-1);
-    }
-    
-    .wrapper{
-    background: var(--Very-Dark-Desaturated-Blue);
-    }
-    
-    .call-to-action{
-    color: var(--Very-Dark-Grayish-Blue);
-    }
-    @media only screen and (max-width: 600px){
-    body {
-        background-image: url(images/bg-mobile-dark.jpg);
-    }
-    }
-    </style>
-    `;
-
-    jsStyle();
-
-    if (input.classList.contains('for-light-theme')) {
-        input.classList.replace('for-light-theme', 'for-dark-theme');
-    } else {
-        input.classList.add('for-dark-theme');
-    }
-    anchor.forEach(a => {
-
-        if (a.classList.contains('light-anchor')) {
-            a.classList.replace('light-anchor', 'dark-anchor');
-        } else {
-            a.classList.add('dark-anchor');
-        }
-    });
-}
-// Light theme
-const lightTheme = () => {
-    style = `
-    <style>
-    body{
-    background-color: var(--Lt-Very-Light-Gray);
-    background-image: url(images/bg-desktop-light.jpg);
-    }
-    form {
-    background-color: var(--Lt-Very-Light-Gray);
-    }
-    
-    input {
-    background-color: var(--Lt-Very-Light-Gray);
-    }
-    
-    .wrapper{
-    background-color: var(--Lt-Very-Light-Gray);
-    }
-
-    .todo-list > li{
-        color: var(--Lt-Very-Dark-Grayish-Blue);
-        border-bottom: 1px solid var(--Lt-Very-Light-Grayish-Blue);
-    }
-
-    
-    .call-to-action{
-    color: var(--Lt-Dark-Grayish-Blue);
-    }
-    @media only screen and (max-width: 600px){
-    body {
-        background-image: url(images/bg-mobile-light.jpg);
-    }
-    }
-    </style>
-    `;
-
-    jsStyle();
 
     // input.classList.add('for-light-theme');
-    input.classList.replace('for-dark-theme', 'for-light-theme');
-    anchor.forEach(a => {
-        a.classList.replace('dark-anchor', 'light-anchor');
-    });
-}
+// input.classList.replace('for-dark-theme', 'for-light-theme');
+// anchor.forEach(a => {
+//     a.classList.replace('dark-anchor', 'light-anchor');
+// });
 
+// Toggle between dark and light mode.
+modeIcons.addEventListener('click', e => {
+    const clickedModeIcon = e.target.alt;
 
-modes.addEventListener('click', e => {
-    if (e.target.classList.contains('dark-mode')) {
-        darkTheme();
-        e.target.style.display = 'none'
-        e.target.nextElementSibling.style.display = 'block';
-    } if (e.target.classList.contains('light-mode')) {
-        lightTheme();
-        e.target.style.display = 'none'
-        e.target.previousElementSibling.style.display = 'block';
+    modeIcons.firstElementChild.classList.toggle('hidden');
+    modeIcons.lastElementChild.classList.toggle('hidden');
+
+    if (clickedModeIcon === "dark mode") {
+         changeTheme()
+    } else {
+        changeTheme()
     }
-})
+});
 
 todos.addEventListener('click', e => {
     e.preventDefault();
@@ -238,5 +164,5 @@ callToAction.addEventListener('click', e => {
     }
 })
 
-darkTheme();
+// darkTheme();
 
