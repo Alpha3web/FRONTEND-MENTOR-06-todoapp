092de5464f7f8bad931c44dab6c88c3ec2cdea73
Akeelah <akeelahsmart@gmail.com>
2024-01-28 00:59:26 +0100

    Transfer logic from static js to server

--- app.js
+++ app.js
@@ -3,24 +3,81 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.set('view engine', 'ejs');
 
-const todoList = ['First todo item'];
+let todoList = [{
+    dateCreated: 'Tuesday',
+    todo: 'First todo item',
+    completed: false
+}];
 
+// Home route, renders all todos
 app.get('/', (req, res) => {
     const numberOfTodos = todoList.length;
 
-    res.render('index', {list: todoList, numb: numberOfTodos});
+    res.render('index', { list: todoList, numb: numberOfTodos });
 });
 
+// Home post route, recieve data from ejs form and create new todo object.
 app.post('/', (req, res) => {
     const item = req.body.newTodo;
-    todoList.push(item);
+    const date = new Date();
+    
+    const newTodoItem = {
+        dateCreated: date.toDateString(),
+        todo: item,
+        completed: false
+    }
+    todoList.push(newTodoItem);
 
     res.redirect('/');
 });
+
+// Completed route, filter all completed todos
+app.get('/completed', (req, res) => {
+
+    const completedTodos = todoList.filter(item => item.completed === true);
+
+    res.render('index', { list: completedTodos, numb: completedTodos.length });
+})
+
+// Active route, filter all active todos.
+app.get('/active', (req, res) => {
+    const activeTodos = todoList.filter(item => item.completed === false);
+
+    res.render('index', { list: activeTodos, numb: activeTodos.length });
+});
+
+// Recieve todo status from frontend js.
+app.post("/todoItem", (req, res) => {
+    const todoIndex = Number(req.body.item);
+    const todoNewState = req.body.state;
+
+    todoList[todoIndex].completed = todoNewState;
+    res.send({ feedback: 'State updated' });
+});
+
+// Delete an item.
+app.post("/todoItem/:todoIndex", (req, res) => {
+    res.redirect('/');
+});
+
+// delete all checked todos
+app.get('/clearCompleted', (req, res) => {
+    for (let i = 0; i < todoList.length; i++) {
+        const todoItem = todoList[i];
+        if (todoItem.completed) {
+            todoList.splice(i, 1);
+        }
+    }
+    
+    res.redirect('/');
+});
+
+
 
 app.listen(3000, () => {
     console.log('Hello dear! Listening at port 3000.');
--- public/static_js/index.js
+++ public/static_js/index.js
@@ -1,15 +1,14 @@
-
 const todos = document.querySelector('.todo-list');
 const callToAction = document.querySelector('.call-to-action');
-const anchor = document.querySelectorAll('.call-to-action a');
 const modeIcons = document.querySelector('.mode-icons');
 
 // To apply theme
 const pageBody = document.querySelector('body');
 const form = document.querySelector('form');
-const input = document.querySelector('input');
+const input = document.querySelector('.light-text-input');
 const todoItems = document.querySelectorAll('.todo-item');
-const wrapper = document.querySelector('.wrapper')
+const wrapper = document.querySelector('.wrapper');
+const anchors = document.querySelectorAll('.light-anchor');
 
 const changeTheme = () => {
     pageBody.classList.toggle('dark-body')
@@ -21,28 +20,11 @@ const changeTheme = () => {
     todoItems.forEach(item => {
         item.classList.toggle('dark-todo-item')
     })
+    anchors.forEach(anchor => {
+        anchor.classList.toggle('dark-anchor');
+    });
 }
 
-// if (input.classList.contains('for-light-theme')) {
-//     input.classList.replace('for-light-theme', 'for-dark-theme');
-// } else {
-//     input.classList.add('for-dark-theme');
-// }
-// anchor.forEach(a => {
-
-//     if (a.classList.contains('light-anchor')) {
-//         a.classList.replace('light-anchor', 'dark-anchor');
-//     } else {
-//         a.classList.add('dark-anchor');
-//     }
-// });
-
-
-    // input.classList.add('for-light-theme');
-// input.classList.replace('for-dark-theme', 'for-light-theme');
-// anchor.forEach(a => {
-//     a.classList.replace('dark-anchor', 'light-anchor');
-// });
 
 // Toggle between dark and light mode.
 modeIcons.addEventListener('click', e => {
@@ -58,33 +40,31 @@ modeIcons.addEventListener('click', e => {
     }
 });
 
-todos.addEventListener('click', e => {
-    e.preventDefault();
 
-    if (e.target.classList.contains('div')) {
-        e.target.style.background = `linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))`;
-        e.target.parentElement.style.textDecoration = `line-through`;
-        e.target.firstChild.style.display = 'block';
-
-        e.target.parentElement.classList.add('comp');
-
-        todoItems();
+const  sendData = async e => {
+    if (e.target.name === 'checkbox'){
+        
+        const data = {item: e.target.id, state: e.target.checked}
+    
+        const response = await fetch('/todoItem', {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        const result = await response.json();
+        console.log("Success! " + result.feedback);
     }
+}
 
-
-    // deleting todos
-
-    if (e.target.classList.contains('delete')) {
-        e.target.parentElement.remove();
-        todoItems();
-    }
-});
+todos.addEventListener('click', sendData);
 
 // managing todos
 
 
 callToAction.addEventListener('click', e => {
-    e.preventDefault();
+    // e.preventDefault();
 
     switch (e.target.hash) {
 
@@ -92,11 +72,7 @@ callToAction.addEventListener('click', e => {
         // all todos
 
         case '#all':
-            Array.from(todos.children).forEach(allTodo => {
-                if (allTodo.classList.contains('hidden')) {
-                    allTodo.classList.remove('hidden');
-                }
-            })
+
             e.target.style.color = 'var(--Bright-Blue)';
             break;
 
@@ -104,43 +80,12 @@ callToAction.addEventListener('click', e => {
 
         case '#completed':
 
-            const completedTodos = () => {
-                Array.from(todos.children)
-                    .filter(completedTodo => {
-                        return completedTodo.className !== 'comp';
-                    })
-                    .forEach(completedTodo => {
-
-                        if (completedTodo.classList.contains('hidden')) {
-                            completedTodo.classList.remove('hidden');
-                        } else {
-                            completedTodo.classList.add('hidden');
-                        }
-                    });
-            }
-
-
-            completedTodos();
             e.target.style.color = 'var(--Bright-Blue)';
             break;
 
         // active todos
 
         case '#active':
-            const activeTodos = () => {
-                Array.from(todos.children)
-                    .filter(activeTodo => {
-                        if (activeTodo.className === 'comp') {
-                            activeTodo.classList.add('hidden');
-                        } else {
-                            activeTodo.classList.remove('hidden');
-                        }
-
-                    });
-            }
-
-
-            activeTodos();
 
             e.target.style.color = 'var(--Bright-Blue)';
             break;
@@ -149,20 +94,7 @@ callToAction.addEventListener('click', e => {
 
 
         case '#clear-completed':
-            const clearCompleted = () => {
-                Array.from(todos.children)
-                    .forEach(clearTodo => {
-
-                        if (clearTodo.classList.contains('comp')) {
-                            clearTodo.remove();
-                        }
-
-                    });
-            }
-            clearCompleted();
-            break;
     }
 })
 
-// darkTheme();
 
